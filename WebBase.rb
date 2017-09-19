37092fb240ec1b644451700e997cf31ab02a45de
MrFunlife <dgfunlife@gmail.com>
2017-09-19 19:57:59 +0700

    webbase

---
+++ WebBase.rb
@@ -0,0 +1,34 @@
+
+1. Web - Base
+
+* HTTP:
+
++ Request
++ Response
++ Header
+
+* HTTPS: bao mat, ma hoa parameter truyen len
+
+* Season: luu gia tri nguoi dung, Tao o phia sever roi tra ve sesaon id cho client. Season co 1 timeout = 30 phut default, sau timeout tao 1 season moi, object luu se bi xoa
+
+
+
+- Cookie: luu gia tri nguoi dung tren trinh duyet, khong duoc luu nhung gia tri can bao mat, co gioi han bo nho, 4MB
+
+- WebSever:
+
+
+
+
+- HTTP Method -> Resful:
+  Get - Truyen parameter qua query string - gioi han 255 ky tu
+  Pots - Truyen parameter qua form-data
+
+- Deployment:
+  1 sever xu ly request va nhieu sever xu ly code
+
+
+
+2. HTML
+3. CSS
+4. JS
